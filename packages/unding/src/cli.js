4857907e2b11dd42140f60ea8d6583354fc44a64
Gustav Hansen <gustav.hansen@strapi.io>
2023-04-11 22:45:24 +0200

    Wrap vite entirely

---
+++ packages/unding/src/cli.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+import { program } from "commander";
+import { fileURLToPath } from 'node:url'
+import { createServer } from 'vite'
+import { join } from 'node:path';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
+async function dev() {
+    const server = await createServer({
+    configFile: join(__dirname, 'vite.config.js'),
+    server: {
+        port: 4000,
+    },
+    })
+
+    await server.listen()
+
+    server.printUrls()
+}
+
+program
+  .command('dev')
+  .action(dev);
+
+program.parse();
