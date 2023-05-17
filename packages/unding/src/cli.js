dcf6f4e5ec14cd267f794272e6189914633e8019
Gustav Hansen <gustav.hansen@strapi.io>
2023-05-17 23:39:12 +0200

    Remove build command

--- packages/unding/src/cli.js
+++ packages/unding/src/cli.js
@@ -1,16 +1,8 @@
 #!/usr/bin/env node
 
 import { program } from "commander";
-import { build as viteBuild } from 'vite'
-import { join } from 'node:path';
 
 import { startServer } from './server.js';
-
-async function build() {
-  await viteBuild({
-    configFile: join(process.cwd(), 'vite.config.js'),
-  })
-}
 
 async function start() {
   await startServer({ cwd: process.cwd() });
@@ -19,10 +11,6 @@ async function start() {
 async function dev() {
   await startServer({ env: 'development', cwd: process.cwd() });
 }
-
-program
-  .command('build')
-  .action(build);
 
 program
   .command('dev')
