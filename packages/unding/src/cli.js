12c0cd1795856243edef50531e119ee70c819e1f
Gustav Hansen <gustav.hansen@strapi.io>
2023-05-18 15:51:49 +0200

    Use vite.build programmatically

--- packages/unding/src/cli.js
+++ packages/unding/src/cli.js
@@ -1,16 +1,35 @@
 #!/usr/bin/env node
 
 import { program } from "commander";
+import { join } from 'node:path';
+import * as vite from 'vite';
 
 import { startServer } from './server.js';
 
-async function start() {
-  await startServer({ cwd: process.cwd() });
+async function build() {
+  await Promise.all([
+    vite.build({
+      configFile: join(process.cwd(), 'vite.config.js'),
+    }),
+
+    vite.build({
+      configFile: join(process.cwd(), 'vite.config.js'),
+      ssr: true,
+    })
+  ]);
 }
 
 async function dev() {
   await startServer({ env: 'development', cwd: process.cwd() });
 }
+
+async function start() {
+  await startServer({ cwd: process.cwd() });
+}
+
+program
+  .command('build')
+  .action(build);
 
 program
   .command('dev')
